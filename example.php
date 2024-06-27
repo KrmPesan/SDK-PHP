574939a9d47abc503167da5f74b22dd3a29a34d2
Juni Yadi <juniyadi94@gmail.com>
2024-06-27 09:10:52 +0700

    Update example.php

--- example.php
+++ example.php
@@ -2,24 +2,36 @@
 
 // panggil file source
 require 'src/ClientV3.php';
+// atau jika menggunakan composer require krmpesan/sdk
+// require "vendor/autoload.php";
 
 // panggil class
 use KrmPesan\ClientV3;
 
-// setting konfigurasi
+// setting konfigurasi menggunakan file token.json
+// arahkan direktori file token.json ke variabel tokenFile
 $wa = new ClientV3([
     'tokenFile' => __DIR__,
 ]);
 
 // print_r($wa->getDevice());
 
-$messages = $wa->getMessages();
-print_r(json_decode($messages, true));
+// $messages = $wa->getMessages();
+// print_r(json_decode($messages, true));
 
-// sending message with template
-// $data = $wa->sendMessageTemplate(
+// sending message with template without parameter
+$data = $wa->sendMessageTemplateText(
+    '081231135699',
+    'promo_juni_2024',
+    'id',
+);
+print_r(json_decode($data, true));
+
+// sending message with template with parameter
+// $data = $wa->sendMessageTemplateText(
 //     '0812xxxxxxx',
 //     'news_notification',
+//     'id',
 //     ['John', 'Produk', 'Produk kami tidak ada masalah sama sekali', 'Krm Pesan'],
 // );
 // print_r(json_decode($data, true));
